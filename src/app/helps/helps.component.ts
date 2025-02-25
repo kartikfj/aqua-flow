54d45758f8a48ce5878339f297672ff32eaa7f45
kartikfj <kartik.p@fjtco.com>
2025-02-25 17:01:01 +0400

    new updated with changes

--- src/app/helps/helps.component.ts
+++ src/app/helps/helps.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './helps.component.css'
 })
 export class HelpsComponent {
-
+  downloadPDF() {
+    const fileUrl = 'assets/manual2.pdf';  // Path to the PDF in the assets folder
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'document.pdf';  // You can change the filename here
+    link.click();
+  }
 }
